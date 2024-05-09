579181b6553cdfe33842104f87484e55700b6871
Your Name <you@example.com>
2024-05-09 12:30:19 +0530

    utils code added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,20 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is luning of port: ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log("MONGO db connection failed!!", err);
+});
+
+
+
+
+
+
+
 
 // const app = express()
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asynHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+export {asynHandler}
+
+
+// const  asynHandler = (fn) => async (err,req, res,next) => {
+
+//     try{
+//     await fn(err,req,res,next)
+//     }catch(error) {
+// res.status(err.code || 500).json({
+//     success: false,
+//     message: err.message
+// })
+//     }
+// }
